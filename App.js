7bdaeaa43eef9b4fd9c4e4b8d694f10ff7f383da
VovaMyb <rimidalvatopayl@gmail.com>
2023-08-31 20:11:45 +0300

    Add screens

--- App.js
+++ App.js
@@ -1,9 +1,3 @@
-import { Text, View } from "react-native";
-
 export default function App() {
-  return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  return;
 }
