e783a7ee222c7b074d04f2f852f70a0277ce85fa
VovaMyb <rimidalvatopayl@gmail.com>
2023-09-30 21:45:03 +0300

    remove comments

--- src/redux/publications/publicationsSlice.js
+++ src/redux/publications/publicationsSlice.js
@@ -3,27 +3,18 @@ import {
   fetchPublications,
   addPublication,
   resetPublications,
-  //   deletePublication,
-  //   editPublication,
 } from "./operations";
 
 export const publicationsSlice = createSlice({
   name: "publications",
   initialState: {
     items: [],
-    // isLoading: false,
-    // error: null,
   },
 
   extraReducers: (builder) => {
     builder
-      // .addCase(fetchPublications.pending, handlePending)
-      // .addCase(addPublication.pending, handlePending)
-      // .addCase(fetchPublications.rejected, handleRejected)
-      // .addCase(addPublication.rejected, handleRejected)
+
       .addCase(fetchPublications.fulfilled, (state, action) => {
-        // state.isLoading = false;
-        // state.error = null;
         state.items = action.payload;
       })
       .addCase(addPublication.fulfilled, (state, action) => {
@@ -32,38 +23,8 @@ export const publicationsSlice = createSlice({
       .addCase(resetPublications.fulfilled, (state, action) => {
         state.items = action.payload;
       });
-    //     .addCase(editPublication.rejected, handleRejected)
-    //     .addCase(deletePublication.pending, handlePending)
-    //     .addCase(deletePublication.rejected, handleRejected)
-    //   .addCase(deletePublication.fulfilled, (state, action) => {
-    //     state.isLoading = false;
-    //     state.error = null;
-
-    //     const deletedContactIndex = state.items.findIndex(
-    //       (contact) => contact.id === action.payload.id
-    //     );
-    //     state.items.splice(deletedContactIndex, 1);
-    //   })
-    //   .addCase(editPublication.fulfilled, (state, action) => {
-    //     const patchedContactId = state.items.findIndex(
-    //       (item) => item.id === action.payload.id
-    //     );
-
-    //     state.isLoading = false;
-    //     state.error = null;
-    //     state.items[patchedContactId] = action.payload;
-    //   });
   },
 });
-
-function handlePending(state) {
-  state.isLoading = true;
-}
-
-function handleRejected(state, action) {
-  state.isLoading = false;
-  state.error = action.payload;
-}
 
 const publicationsReducer = publicationsSlice.reducer;
 export default publicationsReducer;
